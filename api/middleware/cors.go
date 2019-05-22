8e9fe44a27c2a43324981c563cd6957d4a1836c2
David Suarez <david@suarezhouse.net>
2019-05-21 19:36:21 -0500

    CORS Request Issue Fixes - added attributes to avoid

--- api/middleware/cors.go
+++ api/middleware/cors.go
@@ -23,14 +23,16 @@ func preflightHandler(w http.ResponseWriter, r *http.Request) {
 		headers.Add("Vary", "Origin")
 		headers.Add("Vary", "Access-Control-Request-Method")
 		headers.Add("Vary", "Access-Control-Request-Headers")
-		headers.Set("Access-Control-Allow-Origin", "*")
+		headers.Set("Access-Control-Allow-Origin", r.Header.Get("Origin"))
 		headers.Set("Access-Control-Allow-Methods", "OPTIONS,POST,GET,PUT,DELETE,PATCH,HEAD")
 		headers.Set("Access-Control-Allow-Credentials", "true")
-		headers.Set("Access-Control-Allow-Headers", "Content-Type")
+		headers.Set("Access-Control-Allow-Headers", "Content-Length,Content-Type,Origin,Referer,User-Agent")
+		headers.Set("Access-Control-Expose-Headers", "Cache-Control,Content-Length,Content-Type,Date,ETag,Expires,Server,Vary")
 		headers.Set("Access-Control-Max-Age", "3600")
-		headers.Set("X-Content-Type-Options", "nosniff")
-		headers.Set("X-Frame-Options", "SAMEORIGIN")
-		headers.Set("X-XSS-Protection", "1; mode=block")
+		//TODO:  Research these, move to separate middleware if appropriate
+		//headers.Set("X-Content-Type-Options", "nosniff")
+		//headers.Set("X-Frame-Options", "SAMEORIGIN")
+		//headers.Set("X-XSS-Protection", "1; mode=block")
 
 		//log.Printf("in pre-flight, returning")
 		return
