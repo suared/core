160d727425ace4989c4b392d83810e0b042cbaad
David Suarez <david@suarezhouse.net>
2021-12-22 01:44:16 +0000

    On 2nd thought, start private instead

--- queue/queue.go
+++ queue/queue.go
@@ -1,22 +1,22 @@
 package queue
 
 import (
+	"fmt"
 )
 
-//Making these public structs for future flexibility and marking as private which may end up not being smart/ better to expose as needed but...
-
-//Queue - should not be used directly, use StartQueue
+//Starting as private instead and will open up if needed later
+//queue - should not be used directly, use StartQueue
 //example: personQ := StartQueue(Person{}, processPerson, 3)
-type Queue [ msgType any ] struct {
+type queue [ msgType any ] struct {
 	channel chan msgType
 }
 //Send - Use the return from StartQueue to call send
 //example: personQ.Send(Person{Name: "David", Age: 5})
-func(q *Queue[msgType]) Send(msg msgType) {
+func(q *queue[msgType]) Send(msg msgType) {
 	q.channel <- msg
 }
 //Start - should not be used directly, use StartQueue instead
-func(q *Queue[msgType]) Start(fx func(msgType)) {
+func(q *queue[msgType]) start(fx func(msgType)) {
 	for item := range q.channel {
 		fx(item)
 	}
@@ -30,15 +30,16 @@ func(q *Queue[msgType]) Start(fx func(msgType)) {
 //The function run to process the messages is the function passed, in this case processPerson
 //The buffer size shoud be 1 or greater to enable a buffered channel using standard Go channel semantics
 //While "theType" is not used directly, user level code makes more sense this way to not escape the generics semantics outside of the library
-func StartQueue[T any] (theType T, fx func(T), buffer int) (*Queue[T]) {
+func StartQueue[T any] (theType T, fx func(T), buffer int) (*queue[T]) {
 
+	fmt.Println("change test")
 	//Make a channel of the right type
 	channel := make(chan T, buffer)
-	q := &Queue[T]{}
+	q := &queue[T]{}
 	q.channel = channel
 
 	//Start a processor - simple queue, 1 receiver only
-	go q.Start(fx)
+	go q.start(fx)
 	
 	//Build a return to enable sends 
 	return q
--- queue/test/queue_test.go
+++ queue/test/queue_test.go
@@ -1,11 +1,15 @@
-package queue
+package test
 
 import (
 	"fmt"
 	"strconv"
 	"testing"
 	"time"
+
+	"github.com/suared/core/queue"
 )
+
+var testcounter int
 
 type Message struct {
 	Msg string
@@ -26,21 +30,23 @@ func (person Person) String() string {
 
 func processMessage(m Message) {
 	fmt.Println(m.Msg)
+	testcounter++
 }
 
 func processPerson(p Person) {
 	fmt.Println(p.Name + ", age:" + strconv.Itoa(p.Age))
+	testcounter++
 }
 
 func TestQueue(t *testing.T) {
-	msgQ := StartQueue(Message{}, processMessage, 2)
+	msgQ := queue.StartQueue(Message{}, processMessage, 2)
 	msgQ.Send(Message{Msg: "test1"})
 	msgQ.Send(Message{Msg: "test2"})
 	msgQ.Send(Message{Msg: "test3"})
 	msgQ.Send(Message{Msg: "test4"})
 	msgQ.Send(Message{Msg: "test5"})
 
-	personQ := StartQueue(Person{}, processPerson, 3)
+	personQ := queue.StartQueue(Person{}, processPerson, 3)
 	personQ.Send(Person{Name: "David", Age: 5})
 	personQ.Send(Person{Name: "Colleen", Age: 8})
 	personQ.Send(Person{Name: "Charlie", Age: 2})
@@ -50,8 +56,12 @@ func TestQueue(t *testing.T) {
 	personQ.Send(Person{Name: "Mike", Age: 13})
 	personQ.Send(Person{Name: "Fleming", Age: 19})
 
-	//change this to route stdout or t off to count automatically
-	fmt.Println("end test - 14 entries expected, manual count please ")
-	//It is the caller's responsibility to ensure there is time to complete processing
-	time.Sleep(time.Second * 3)
+	//It is the caller's responsibility to ensure there is time to complete processing, for this simple case...
+	time.Sleep(time.Second * 1)
+
+	//confirm all processed
+	if testcounter != 13 {
+		t.Error("Expected 13 items, have " + strconv.Itoa(testcounter))
+	}
+
 }
